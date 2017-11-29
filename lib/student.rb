1b6d3e3b5fa9a55c464a016c55891c443d4aac13
walkingalchemy <walkingalchemy@gmail.com>
2017-11-29 14:45:18 -0500

    Define Student .students_below_12th_grade to query the DB for all students with a grade below 12 and make new student instances out of them

--- lib/student.rb
+++ lib/student.rb
@@ -50,6 +50,16 @@ class Student
     DB[:conn].execute(sql).map {|row| self.new_from_db(row)}
   end
 
+  def self.first_X_students_in_grade_10(x)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 10
+      LIMIT ?
+    SQL
+    DB[:conn].execute(sql, x).map {|row| self.new_from_db(row)}
+  end
+
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade)
