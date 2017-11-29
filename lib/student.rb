9f178ea866622f26a35caf175976f5d005a8710a
walkingalchemy <walkingalchemy@gmail.com>
2017-11-29 14:37:49 -0500

    Define Student .count_all_students_in_grade_9 to return an integer representing the total number of students in the DB with a grade of 9

--- lib/student.rb
+++ lib/student.rb
@@ -32,7 +32,14 @@ class Student
     DB[:conn].execute(sql, name).map {|row| self.new_from_db(row)}.first
   end
 
-  
+  def self.count_all_students_in_grade_9
+    sql = <<-SQL
+      SELECT COUNT(id)
+      FROM students
+      WHERE grade = 9
+    SQL
+    DB[:conn].execute(sql).map {|row| self.new_from_db(row)}
+  end
 
   def save
     sql = <<-SQL
